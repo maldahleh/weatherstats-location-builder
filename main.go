803785b06b41d0ae0c1c6475ff77d2bf781387b8
Mohammed Al-Dahleh <maldahleh@gmail.com>
2019-10-13 18:58:13 -0400

    Scraping improvements

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "weatherstatsLocations/scraper"
+import (
+	"log"
+	"os"
+
+	"weatherstatsLocations/scraper"
+)
 
 func main() {
 	//err := downloader.DownloadFile(
@@ -11,5 +16,18 @@ func main() {
 	//	log.Fatal(err)
 	//}
 
-	scraper.Scrape()
+	setupLogging()
+	scraper.Scrape("ON")
+}
+
+func setupLogging() {
+	file, err := os.OpenFile("info.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Println("test")
 }
--- scraper/web_scraper.go
+++ scraper/web_scraper.go
@@ -2,31 +2,60 @@ package scraper
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
 const rootUrl string = "http://dd.weatheroffice.ec.gc.ca/climate/observations/daily/csv/"
+var provinces = [...]string {
+	"AB",
+	"BC",
+	"MB",
+	"NB",
+	"NL",
+	"NS",
+	"NT",
+	"NU",
+	"ON",
+	"PE",
+	"QC",
+	"SK",
+	"YT",
+}
 
-func Scrape() {
+func Scrape(province string) {
 	c := colly.NewCollector(
 		colly.AllowedDomains(
 			"dd.weatheroffice.ec.gc.ca",
 			"www.dd.weatheroffice.ec.gc.ca",
 		),
+		colly.AllowURLRevisit(),
 		colly.CacheDir("./eccc_cache"),
+		colly.MaxDepth(1),
+		colly.UserAgent("Mozilla/5.0"),
 	)
 
-	// Find and visit all links
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+		path := e.Attr("href")
+		if !strings.Contains(path, ".csv") {
+			return
+		}
+
+		split := strings.Split(path, "_")
+		if len(split) != 6 {
+			return
+		}
+
+		fmt.Println(split)
 	})
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
 
-	err := c.Visit(rootUrl)
+	err := c.Visit(rootUrl + province + "/")
 	if err != nil {
-		fmt.Print("Failed to visit: " + rootUrl + ", error: " + err.Error())
+		fmt.Println("Failed to visit:", rootUrl, "with error:", err.Error())
 	}
 }
